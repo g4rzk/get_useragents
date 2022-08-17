fc8eca5e3b6d19a025b241044eed411158e98a66
Angga Kurniawan <67410461+g4rzk@users.noreply.github.com>
2022-08-17 11:26:23 +0700

    Update test.py

--- example/test.py
+++ example/test.py
@@ -1,4 +1,11 @@
 from get_useragents import *
 
-a = UserAgents()
-a.GetUserAgent()
+# you can change the limit
+ua = UserAgents(limit=100)
+
+# fungsi untuk mendapatkan semua ua sesuai limit dari GetListUserAgents
+user_agents = ua.GetUserAgents()
+# function for get random ua from GetListUserAgents
+random_ua = ua.RandomUserAgents()
+
+print(random_ua)
