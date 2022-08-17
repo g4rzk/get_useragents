86b24be6ca33d4a8fe8f2103afb3b3213267471c
Angga Kurniawan <67410461+g4rzk@users.noreply.github.com>
2022-08-17 11:25:52 +0700

    Add files via upload

--- get_useragents/useragents.py
+++ get_useragents/useragents.py
@@ -6,21 +6,29 @@ import requests
 from bs4 import BeautifulSoup
 
 class UserAgents:
-	def __init__(self):
-		self.url = "https://useragents.io/random?limit=1500"
+	def __init__(self, limit=None):
+		self.user_agents = []
+		self.url = f"https://useragents.io/random?limit={limit}"
 		self.ua = "Mozilla/5.0 (Linux; Android 5.1; A70 Build/LMY47I; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/43.0.2357.121 Mobile Safari/537.36"
-	
-	def GetUserAgent(self):
+		self.GetListUserAgents()
+
+	def GetListUserAgents(self):
 		try:
-			a = requests.get(
+			get = requests.get(
 				self.url, 
 				headers={"user-agent": self.ua}, 
 				timeout=1
 			).text
-			b = BeautifulSoup(a, "html.parser")
-			for x in b.find_all("td"):
+			parser = BeautifulSoup(get, "html.parser")
+			for x in parser.find_all("td"):
 				z = re.search('\<a\ href\=\"\/(.*?)\" title="(.*?)">(.*?)<\/a>', str(x))
-			
-			print(z.group(2)) 
+				self.user_agents.append(f"{z.group(2)}\n")
+
 		except:
 			pass
+
+	def GetUserAgents(self):
+		return "".join(self.user_agents)
+
+	def RandomUserAgents(self):
+		return random.choice(self.user_agents)
