4e05a30a01cb7e9145d197796f2ba3579e60c463
Angga Kurniawan <kinganggaxd@gmail.com>
2022-08-17 19:37:31 +0700

    Add files via upload

---
+++ get_useragents/settings.py
@@ -0,0 +1,3 @@
+# settings.py
+BASE_URL = "https://useragents.io/"
+USER_AGENT = "Mozilla/5.0 (Linux; Android 5.1; A70 Build/LMY47I; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/43.0.2357.121 Mobile Safari/537.36"
--- get_useragents/useragents.py
+++ get_useragents/useragents.py
@@ -1,34 +1,38 @@
 #!/usr/bin/python
-
 import re
 import random
 import requests
 from bs4 import BeautifulSoup
+from get_useragents import settings
 
 class UserAgents:
 	def __init__(self, limit=None):
 		self.user_agents = []
-		self.url = f"https://useragents.io/random?limit={limit}"
-		self.ua = "Mozilla/5.0 (Linux; Android 5.1; A70 Build/LMY47I; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/43.0.2357.121 Mobile Safari/537.36"
+		self.url = (f"https://useragents.io/random?limit={limit}")
 		self.GetListUserAgents()
 
 	def GetListUserAgents(self):
 		try:
 			get = requests.get(
 				self.url, 
-				headers={"user-agent": self.ua}, 
-				timeout=1
+				headers={"user-agent": settings.USER_AGENT}, 
+				timeout=5
 			).text
 			parser = BeautifulSoup(get, "html.parser")
 			for x in parser.find_all("td"):
 				z = re.search('\<a\ href\=\"\/(.*?)\" title="(.*?)">(.*?)<\/a>', str(x))
-				self.user_agents.append(f"{z.group(2)}\n")
-
+				self.user_agents.append(f"{z.group(2)}")
 		except:
 			pass
 
 	def GetUserAgents(self):
-		return "".join(self.user_agents)
+		try:
+			return "\n".join(self.user_agents)
+		except IndexError:
+			exit("* Error detected spam please turn on airplane mode.")
 
 	def RandomUserAgents(self):
-		return random.choice(self.user_agents)
+		try:
+			return random.choice(self.user_agents)
+		except IndexError:
+			exit("* Error detected spam please turn on airplane mode.")
