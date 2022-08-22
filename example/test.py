320cb113fde3eb4a594f3f72fb086ad7f35080d0
Angga Kurniawan <kinganggaxd@gmail.com>
2022-08-22 19:08:54 +0700

    Add files via upload

--- example/test.py
+++ example/test.py
@@ -1,9 +1,9 @@
-from get_useragents import *
+from get_useragents import useragents
 
 # you can change the limit
-ua = UserAgents(limit=100)
+ua = useragents.UserAgents(limit=500)
 
-# fungsi untuk mendapatkan semua ua sesuai limit dari GetListUserAgents
+# function to get all ua as per limit from GetListUserAgents
 user_agents = ua.GetUserAgents()
 # function for get random ua from GetListUserAgents
 random_ua = ua.RandomUserAgents()
