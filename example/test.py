a03218f87e874961f3341b860faff448dca0a42e
Angga Kurniawan <kinganggaxd@gmail.com>
2022-11-21 22:55:01 +0700

    Add files via upload

--- example/test.py
+++ example/test.py
@@ -1,11 +1,12 @@
 from get_useragents import useragents
 
-# you can change the limit
-ua = useragents.UserAgents(limit=500)
+# you can change string limit '100' to max limit which is '1000'
+# to see available devices, check: https://github.com/g4rzk/get_useragents#list-device
+ua = useragents.Get_UA(device="chrome", limit=100)
 
-# function to get all ua as per limit from GetListUserAgents
-user_agents = ua.GetUserAgents()
-# function for get random ua from GetListUserAgents
+# to get all user agents according to the limit
+all_ua = ua.GetUserAgents()
+# to get a random user agent from limit
 random_ua = ua.RandomUserAgents()
 
 print(random_ua)
